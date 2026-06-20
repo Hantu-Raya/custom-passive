a6195e50c416fc1ff5b88148e25338cd377dc5ff
Rosli Zaqwan <66868007+Hantu-Raya@users.noreply.github.com>
2026-06-20 16:26:46 +0800

    ci: update Pages actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
   site: 'https://hantu-raya.github.io',
   base: '/custom-passive/',
-  integrations: [preact()]
+  integrations: [preact()],
+  vite: {
+    resolve: {
+      alias: {
+        module: fileURLToPath(new URL('./src/lib/nodeModuleShim.js', import.meta.url))
+      }
+    }
+  }
 });
